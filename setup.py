76f0a901d3e2c75904c825528154792894f8d351
Matt Griswold <grizz@20c.com>
2015-08-11 22:48:50 +0000

    fix setup

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ version = open('config/VERSION').read().strip()
 setup(
     name='twentyc.rpc',
     version=open('config/VERSION').read().rstrip(),
-    author='Twentieth Century',
+    author='20C',
     author_email='code@20c.com',
     description='client for 20c\'s RESTful API',
-    long_description=open('README.rst').read(),
+    long_description=open('README.md').read(),
     license='LICENSE',
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -19,10 +19,10 @@ setup(
     ],
     packages=['twentyc.rpc'],
     namespace_packages=['twentyc'],
-    url = 'https://github.com/20c/twentyc.rpc',
-    download_url = 'https://github.com/20c/twentyc.rpc/%s'%version,
+    url='https://github.com/20c/twentyc.rpc',
+    download_url='https://github.com/20c/twentyc.rpc/%s'%version,
     include_package_data=True,
-    maintainer='Twentieth Century',
+    maintainer='20C',
     maintainer_email='code@20c.com',
     zip_safe=False
 )
